8490d2ed17d6b294fd2af1dd5e25bc43160d4df6
Bogdan-Doctorian <egor1414148@gmail.com>
2023-04-28 15:36:19 +0300

    added addPlacePopup

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,18 @@
 let editBtn = document.querySelector('.profile__edit-btn');
-let closeBtn = document.querySelector('.popup__close-button');
+let closeEditPopupBtn = document.querySelector('#closeEditPopup');
 let editPopup = document.querySelector('#editPopup');
 let profileName = document.querySelector('.profile__name');
 let profileBio = document.querySelector('.profile__bio');
-let formElement = document.querySelector('.popup__form');
+let editForm = document.querySelector('#editForm');
 let nameInput = document.querySelector('#nameInput');
 let jobInput = document.querySelector('#jobInput');
+
+let addPlaceBtn = document.querySelector('.profile__button');
+let closeAddPopupBtn = document.querySelector('#closeAddPopup');
+let addPlacePopup = document.querySelector('#addPlacePopup');
+let addPlaceForm = document.querySelector('#addPlaceForm');
+let placeNameInput = document.querySelector('#placeNameInput');
+let placeLinkInput = document.querySelector('#placeLinkInput');
 
 const initialCards = [
   {
@@ -52,5 +59,11 @@ function handleFormSubmit(evt, popup) {
 }
 
 editBtn.addEventListener('click',() => openPopup(editPopup));
-closeBtn.addEventListener('click',() => closePopup(editPopup));
-formElement.addEventListener('submit',(event) => handleFormSubmit(event, editPopup));
+closeEditPopupBtn.addEventListener('click',() => closePopup(editPopup));
+editForm.addEventListener('submit',(event) => handleFormSubmit(event, editPopup));
+
+addPlaceBtn.addEventListener('click',() => openPopup(addPlacePopup));
+closeAddPopupBtn.addEventListener('click', () => closePopup(addPlacePopup));
+addPlaceForm.addEventListener('submit', (event) => handleFormSubmit(event, addPlacePopup));
+
+
