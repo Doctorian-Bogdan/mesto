2e1ed478fc93ca8279e11ce8ac85fbe035aa3ed0
Bogdan-Doctorian <egor1414148@gmail.com>
2023-04-28 20:56:48 +0300

    added logic of adding places

--- scripts/index.js
+++ scripts/index.js
@@ -40,6 +40,16 @@ const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
+let cardTemplate = document.querySelector('#galleryCard').content;
+let gallery = document.querySelector('.gallery');
+
+initialCards.forEach(function (cardInfo) {
+  let galleryCard = cardTemplate.cloneNode(true);
+  galleryCard.querySelector('.gallery__image').src = cardInfo.link;
+  galleryCard.querySelector('.gallery__image').alt = cardInfo.name;
+  galleryCard.querySelector('.gallery__title').textContent = cardInfo.name;
+  gallery.append(galleryCard);
+});
 
 function openPopup(popup) {
   popup.classList.add('popup_opened');
@@ -58,12 +68,31 @@ function handleFormSubmit(evt, popup) {
   popup.classList.remove('popup_opened');
 }
 
+function addPlace(evt, popup) {
+  evt.preventDefault();
+  let galleryCard = cardTemplate.cloneNode(true);
+  if (placeLinkInput.value && placeNameInput.value) {
+    galleryCard.querySelector('.gallery__image').src = placeLinkInput.value;
+    galleryCard.querySelector('.gallery__image').alt = placeNameInput.value;
+    galleryCard.querySelector('.gallery__title').textContent = placeNameInput.value;
+    gallery.prepend(galleryCard);
+  } else {
+    galleryCard.querySelector('.gallery__image').src = 'https://animals.pibig.info/uploads/posts/2023-03/thumbs/1680281518_animals-pibig-info-p-kotik-prosit-zhivotnie-pinterest-1.jpg';
+    galleryCard.querySelector('.gallery__image').alt = 'котик';
+    galleryCard.querySelector('.gallery__title').textContent = 'Тут пусто';
+    gallery.prepend(galleryCard);
+  }
+  popup.classList.remove('popup_opened');
+  placeNameInput.value = null;
+  placeLinkInput.value = null;
+}
+
 editBtn.addEventListener('click',() => openPopup(editPopup));
 closeEditPopupBtn.addEventListener('click',() => closePopup(editPopup));
 editForm.addEventListener('submit',(event) => handleFormSubmit(event, editPopup));
 
 addPlaceBtn.addEventListener('click',() => openPopup(addPlacePopup));
 closeAddPopupBtn.addEventListener('click', () => closePopup(addPlacePopup));
-addPlaceForm.addEventListener('submit', (event) => handleFormSubmit(event, addPlacePopup));
+addPlaceForm.addEventListener('submit',(event) => addPlace(event, addPlacePopup));
 
 
